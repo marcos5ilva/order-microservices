a3688e11df026f9d74287403f6321aa3b77e12ca
marcos5ilva <marcos.silva79@gmail.com>
2024-08-24 12:10:49 -0400

    chore: add entity classes

---
+++ notifications-service/src/main/java/com/msilva/notifications_service/entity/Order.java
@@ -0,0 +1,27 @@
+package com.msilva.notifications_service.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.msilva.notifications_service.entity.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Order {
+    private UUID id = UUID.randomUUID();
+    private String client;
+    private List<OrderItem> orderItems = new ArrayList<>();
+    private Double totalValue;
+    private String emailNotification;
+    private Status status = Status.PENDING;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime onCreated = LocalDateTime.now();
+
+}
